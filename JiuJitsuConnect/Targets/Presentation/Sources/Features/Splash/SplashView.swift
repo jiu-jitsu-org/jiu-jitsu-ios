49cd117b8d7fbe5279ba5cf99298bee850e2a616
SuniDev <sun-918@naver.com>
2026-05-18 15:13:03 +0900

    fix: 스플래시 화면을 런치스크린과 동일한 외형으로 변경

--- JiuJitsuConnect/Targets/Presentation/Sources/Features/Splash/SplashView.swift
+++ JiuJitsuConnect/Targets/Presentation/Sources/Features/Splash/SplashView.swift
@@ -9,13 +9,18 @@ public struct SplashView: View {
     }
     
     public var body: some View {
-        ZStack {
-            // TODO: 스플래쉬 배경 및 로고 이미지 추가
-            Color.blue.ignoresSafeArea()
-            Text("Oss")
-                .font(.largeTitle)
-                .foregroundStyle(.white)
+        // LaunchScreen.storyboard와 동일한 외형 유지를 위해
+        // 시스템 폰트/원색을 사용한다 (런치스크린은 커스텀 폰트/토큰 로드 불가).
+        GeometryReader { proxy in
+            ZStack {
+                Color.white
+                Text("OSS")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundStyle(.black)
+                    .position(x: proxy.size.width / 2, y: proxy.size.height / 3)
+            }
         }
+        .ignoresSafeArea()
         .onAppear {
             store.send(.view(.onAppear))
         }
